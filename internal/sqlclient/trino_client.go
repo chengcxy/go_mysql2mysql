7b8b1f01334f57af0d449687f4f6347d6137126e
xinyao.cheng <xinyao.cheng@zerone.com.cn>
2024-04-15 19:54:38 +0800

    add trino reader
    Signed-off-by: xinyao.cheng <xinyao.cheng@zerone.com.cn>

--- internal/sqlclient/trino_client.go
+++ internal/sqlclient/trino_client.go
@@ -4,11 +4,10 @@ import (
 	"database/sql"
 	"errors"
 	"fmt"
-	"strings"
 	configor "github.com/chengcxy/go_mysql2mysql/config"
 	_ "github.com/trinodb/trino-go-client/trino"
+	"strings"
 )
-
 
 var QUERY_HIVE_TABLE_COLUMNS = `
 SELECT 
@@ -22,6 +21,7 @@ join hive.COLUMNS_V2 as T3 on T2.CD_ID=T3.CD_ID
 WHERE (T.NAME=? and T1.TBL_NAME=?)
 ORDER BY T.NAME,T1.TBL_NAME,T3.INTEGER_IDX
 `
+
 type TrinoClient struct {
 	MysqlClient
 	metaClient SqlClient
@@ -34,7 +34,7 @@ func NewTrinoClient(config *configor.Config, key string) (*TrinoClient, error) {
 	}
 	m := conf.(map[string]interface{})
 	//http://用户名@host:port?catalog=hive&schema=dw
-	dsn := fmt.Sprintf("http://%s@(%s:%d)?catalog=%s&schema=%s",
+	dsn := fmt.Sprintf("http://%s@%s:%d?catalog=%s&schema=%s",
 		m["user"].(string),
 		m["host"].(string),
 		int(m["port"].(float64)),
@@ -42,18 +42,16 @@ func NewTrinoClient(config *configor.Config, key string) (*TrinoClient, error) {
 		m["schema"].(string),
 	)
 	db, err := sql.Open("trino", dsn)
-	metaClient,_ := NewMysqlClient(config,"mysql.hive_meta")
+	metaClient, _ := NewMysqlClient(config, "mysql.hive_meta")
 	return &TrinoClient{
 		MysqlClient: MysqlClient{Db: db},
-		metaClient:metaClient,
+		metaClient:  metaClient,
 	}, err
 }
 
-
-
 //表的元数据信息 数据库名 表名 字段列表
 func (tc *TrinoClient) GetTableMeta(own_app, db_name, table_name string) (*TableMeta, error) {
-	rows_list, _, err := tc.metaClient.Query(QUERY_HIVE_TABLE_COLUMNS,db_name,table_name)
+	rows_list, _, err := tc.metaClient.Query(QUERY_HIVE_TABLE_COLUMNS, db_name, table_name)
 	if err != nil {
 		return &TableMeta{}, err
 	}
@@ -63,12 +61,11 @@ func (tc *TrinoClient) GetTableMeta(own_app, db_name, table_name string) (*Table
 
 	}
 	tm := &TableMeta{
-		OwnApp:        own_app,
-		DbName:        db_name,
-		TableName:     table_name,
-		Fields:        fields,
+		OwnApp:    own_app,
+		DbName:    db_name,
+		TableName: table_name,
+		Fields:    fields,
 	}
 	tc.metaClient.Close()
 	return tm, nil
 }
-

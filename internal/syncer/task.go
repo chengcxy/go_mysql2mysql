29d108ac98fa79a72c91f35b902af656baf39bc9
xinyao.cheng <xinyao.cheng@zerone.com.cn>
2024-04-15 18:03:17 +0800

    add task_sync_manager.sql
    Signed-off-by: xinyao.cheng <xinyao.cheng@zerone.com.cn>

--- internal/syncer/task.go
+++ internal/syncer/task.go
@@ -1,18 +1,17 @@
 package syncer
 
 type TaskInfo struct {
-	Id           string `json:"id"`            // 统计任务id
+	Id           string `json:"id"`            // 任务id
 	FromApp      string `json:"from_app"`      // 读取的业务系统
 	FromDbType   string `json:"from_db_type"`  // 读取的数据源类型mysql/oracle等
 	FromDb       string `json:"from_db"`       // 读取的数据库
-	FromTable    string `json:"from_table"`    // 读取表
+	FromTable    string `json:"from_table"`    // 读取的表
 	ToApp        string `json:"to_app"`        // 写入的业务系统
 	ToDbType     string `json:"to_db_type"`    // 写入数据源类型
 	ToDb         string `json:"to_db"`         // 写入数据库
 	ToTable      string `json:"to_table"`      // 写入数据表
-	Params       string `json:"params"`        // 规则
+	Params       string `json:"params"`        // 参数
 	OnlineStatus string `json:"online_status"` // 在线状态0统计1不统计
-	WriteMode    string `json:"write_mode"`    // 写入模式replace/insert/update/delete/append/overwrite
 	TaskDesc     string `json:"task_desc"`     // 统计描述
 	Owner        string `json:"owner"`         // 开发所属人
 	TaskStatus   string `json:"task_status"`   // 任务状态
